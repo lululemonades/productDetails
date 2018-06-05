66d04d5fb02f2ac1e714e05525447851571d8572
Kianna <kiannaquach.kq@gmail.com>
2018-06-05 12:02:25 -0700

    Fix airbnb style errors

--- client/components/ProductDetails.jsx
+++ client/components/ProductDetails.jsx
@@ -1,54 +1,53 @@
-import React from'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+// import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 class ProductDetails extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      products: []
-    }
+      products: [],
+    };
   }
   componentDidMount() {
     axios.get('/productDetails')
-    .then((response) => {
-      console.log(response.data)
-      this.setState ({
-        products: response.data
+      .then((response) => {
+        // console.log(response.data);
+        this.setState({
+          products: response.data,
+        });
       })
-    })
-    .catch((error) => {
-      console.log('your get has an error', error);
-    })
+      .catch((error) => {
+        console.log('your get has an error', error);
+      });
   }
 
   render() {
-    return(
+    return (
       <div>
-          <h1>lululemonades product details</h1>
-      <div className="product-details">
+        <h1>lululemonades product details</h1>
+        <div className="product-details">
           {
-            this.state.products.slice(0,1).map((product, i) => {
-              return (
+            this.state.products.slice(0, 1).map(product => (
+              <div>
                 <div>
-                  <div>
-                    <h2>{product.title}</h2>
-                    <div>{product.price}</div>
-                  </div>
+                  <h2>{product.title}</h2>
+                  <div>{product.price}</div>
                 </div>
-              );
-            });
+              </div>
+              ))
           }
         </div>
-        </div>
+      </div>
     );
-  };
+  }
 }
 
 // still working on components
-export default ProductDetails; 
-  // {
-  //   products.map((product, i) => {
-  //     return <div>{product}</div>;
-  //   })
-  // }
+export default ProductDetails;
+
+// {
+//   products.map((product, i) => {
+//     return <div>{product}</div>;
+//   })
+// }
