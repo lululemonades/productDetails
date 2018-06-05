50a7841df5e927f4d459332db02bb52a3d5c5e0c
Kianna <kiannaquach.kq@gmail.com>
2018-06-05 12:02:48 -0700

    Fix airbnb style errors

--- databases/index.js
+++ databases/index.js
@@ -1,50 +1,48 @@
 const mongoose = require('mongoose');
 const itemDetails = require('./mockData');
 
-mongoose.connect('mongodb://localhost:27017/productDetails', (err, db) => {
-    if (err) {
-        console.log('YOUR DB IS NOT CONNECTED')
-    } else {
-        console.log('YOUR DB IS CONNECTED!!');
-    }
+mongoose.connect('mongodb://localhost:27017/productDetails', (err) => {
+  if (err) {
+    console.log('YOUR DB IS NOT CONNECTED');
+  } else {
+    console.log('YOUR DB IS CONNECTED!!');
+  }
 });
 
-let productSchema = mongoose.Schema({
-    "title": String,
-    "price": String,
-    "description": String,
-    "color": String, // Will change to an array of multiple color strings
-    "size": Number, // Will change to an array of multiple size numbers
-    "id": Number
+const productSchema = mongoose.Schema({
+  title: String,
+  price: String,
+  description: String,
+  color: String, // Will change to an array of multiple color strings
+  size: Number, // Will change to an array of multiple size numbers
+  id: Number,
 });
 
-let ProductDetail = mongoose.model('ProductDetail', productSchema);
+const ProductDetail = mongoose.model('ProductDetail', productSchema);
 // console.log(itemDetails)
 // iterate through item
-for (var i = 0; i < itemDetails.length; i++) {
-    // console.log(itemDetails[i])
-    (function() {
-        var products = new ProductDetail({
-            "title": itemDetails[i].title,
-            "price": itemDetails[i].price,
-            "description": itemDetails[i].description,
-            "color": itemDetails[i].color,
-            "size": itemDetails[i].size,
-            "id": itemDetails[i].id
-        });
-    })()
-}
+itemDetails.forEach((item) => {
+  // (function () {
+  const products = new ProductDetail({
+    title: item.title,
+    price: item.price,
+    description: item.description,
+    color: item.color,
+    size: item.size,
+    id: item.id,
+  });
+  // }());
 
-products.save()
-    .then((data) => {
-        // console.log('YOU INSERT TO DB', data)
-    }).catch((err) => {
-        // console.log('YOU FAILED INSERT', err)
-    });
-
+  products.save();
+  // .then((data) => {
+  //   // console.log('YOU INSERT TO DB', data)
+  // }).catch((err) => {
+  //   // console.log('YOU FAILED INSERT', err)
+  // });
+});
+// console.log(item)
 
 
 module.exports = {
-    retrieve,
-    ProductDetail
+  ProductDetail,
 };
