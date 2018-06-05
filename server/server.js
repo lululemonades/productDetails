4158bd22f7f6b6be460b806c9a31627330ab2ea7
Kianna <kiannaquach.kq@gmail.com>
2018-06-05 12:03:50 -0700

    Fix airbnb style errors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+
 const app = express();
 const db = require('../databases/index.js');
 
@@ -10,18 +11,18 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(port, () => {
-    console.log(`server running at: http://localhost:${port}`);
+  console.log(`server running at: http://localhost:${port}`);
 });
 
 app.get('/productDetails', (req, res) => {
-    db.ProductDetail.find()
-        .then((data) => {
-            // console.log('YOUR DATA', data);
-            res.send(data)
-            res.end()
-        }).catch((err) => {
-            // console.log('CANNOT RETRIEVE FROM DB')
-        })
+  db.ProductDetail.find()
+    .then((data) => {
+      // console.log('YOUR DATA', data);
+      res.send(data);
+      res.end();
+    }).catch((err) => {
+      console.log('CANNOT RETRIEVE FROM DB', err);
+    });
 });
 
 // server details
