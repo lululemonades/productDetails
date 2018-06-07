50b06cb6a95ddca4f39ae15b0f564a52b7c0f98b
Kianna <kiannaquach.kq@gmail.com>
2018-06-06 22:47:53 -0700

    Add colors component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import axios from 'axios';
 import Colors from './Colors.jsx';
 import Sizes from './Sizes.jsx';
+import ItemMaterial from './ItemMaterial.jsx';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -31,16 +33,40 @@ class App extends React.Component {
             this.state.products.map(product => (
               <div>
                 <h1>{product.title}</h1>
-                <div className="price">{product.price}</div>
+                <span className="price">{product.price}</span>
                 <div>Why we made this</div>
                 <p>{product.description}</p>
                 <div className="border" />
+
                 <div>
                   <Colors colors={product.color} />
                 </div>
+
                 <div>
                   <Sizes sizes={product.size} />
                 </div>
+
+                <div>
+                  <div>
+                    <button className="button" id="bag">ADD TO BAG</button>
+                  </div>
+
+                  <div>
+                    <button className="button" id="store">FIND IN STORE</button>
+                  </div>
+                </div>
+
+                <div className="social">
+                  <button><img src="share.png" id="share" /> Share</button>
+                  <span className="vertical-line" />
+                  <button><img src="square-bubble.png" id="share" /> Live Chat</button>
+                  <button><img src="star.png" id="share" /> Reviews</button>
+                </div>
+
+                <div>
+                  <ItemMaterial fabric={product.fabric} care={product.care} features={product.features} />
+                </div>
+
               </div>
             ))
          }
